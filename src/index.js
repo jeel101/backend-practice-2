354309061f8b6fd222816cf5519752396226f543
Jeel <shahjeel459@gmail.com>
2024-12-07 14:23:23 +0530

    added api error handling

--- src/index.js
+++ src/index.js
@@ -6,8 +6,15 @@ dotenv.config({
     path: './env'
 })
 
-connect();
-
+connect()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("mongoDB connection failed!", err);
+    })
 
 
 
